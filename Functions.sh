a9cb3b0b0f4262984361a156f0852a40878371bb
Vinod <vinodsv1618@gmail.com>
2026-01-29 23:00:21 +0530

    hello

--- Functions.sh
+++ Functions.sh
@@ -5,24 +5,24 @@ Logs_file="/var/log/shell-script/$0.log"
 
 
 if [ $(id -u) -ne 0 ]; then
-  echo "please run this script with root user access"
+  echo "please run this script with root user access" | tee -a $Logs_file
   exit 1
 fi
 
 validate (){
 
 if [ $1 -ne 0 ]; then
-  echo "$2 installation failed"
+  echo "$2 installation failed" | tee -a $Logs_file
   exit 1
 else
-  echo "$2 installation is success"
+  echo "$2 installation is success" | tee -a $Logs_file
 fi
 
 }
 
 dnf install nginx -y &>> $Logs_file
 validate $? "nginx" 
-dnf install mysql -y  &>> $Logs_file
+dnf install mysql -y  &>> $Logs_file 
 validate $? "mysql"
 dnf install nodejs -y  &>> $Logs_file
 validate $? "nodejs"
