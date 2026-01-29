d5a1f1b6406050764bb9bb2e603d12b1ac402285
Vinod <vinodsv1618@gmail.com>
2026-01-29 11:50:09 +0530

    hello

--- Use_of_paranthesis.sh
+++ Use_of_paranthesis.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-names=("vinod" "suresh")
+names= ("vinod" "suresh")
 today=$(date)
 echo "$today"
+
+#another example
+
+Script_started=$(date +%s)
+echo "$today"
+
+sleep  10
+
+Script_end=$(date +%s)
+
+Timetaken=$((Script_end-Script_started))
+echo "script took $Timetaken seconds to run"
