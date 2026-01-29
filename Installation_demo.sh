dd31e19951f7309ce4983317eda38b2f25cf37c7
Vinod <vinodsv1618@gmail.com>
2026-01-29 18:13:20 +0530

    hello

---
+++ Installation_demo.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+if [ $(id -u)!=0 ]
+   echo "please run this script with root user access"
+   exit 1
+
+fi
+
+dnf install nginx -y
+
+if ( $? == 1 )
+
+    echo "Nginx installation is failed"
+    exit 1
+else 
+   
+    echo "installation is success"
+ fi
