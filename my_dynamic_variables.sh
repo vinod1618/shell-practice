1d8da117af6172c545b89d1fc86858c23b94f483
Vinod <vinodsv1618@gmail.com>
2026-01-29 11:12:49 +0530

    hello

--- my_dynamic_variables.sh
+++ my_dynamic_variables.sh
@@ -4,6 +4,8 @@ echo "enter your name:"
 read USER_NAME
 echo "enter your email id:"
 read EMAIL_ID
-
+echo "enter your password"
+read -s PASSWORD                 # -s option will make you not display what you are typing when you are entereing the characters on the screen
 echo "your name is: $USER_NAME"
 echo "Your emai id is:$EMAIL_ID "
+echo "your password is :$PASSWORD"
