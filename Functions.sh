9b8cf6e938e333c29e94630465fc00663bb41106
Vinod <vinodsv1618@gmail.com>
2026-01-29 19:54:43 +0530

    hello

---
+++ Functions.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ $(id -u) -ne 0 ]; then
+  echo "please run this script with root user access"
+  exit 1
+fi
+
+dnf install nginx -y
+validate ($?, "nginx") 
+
+#dnf install mysql -y
+
+#dnf install nodejs -y
+
+validate (){
+if [ $1 -ne 0 ]; then
+  echo "$2 installation failed"
+  exit 1
+else
+  echo "installation is success"
+fi
+}
