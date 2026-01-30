465a9576ad41d58dc7900dc7c42ea470f6119894
Vinod <vinodsv1618@gmail.com>
2026-01-30 14:10:17 +0530

    hello

--- Trap_command.sh
+++ Trap_command.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e  #ERR
-trap "echo "There is an error in line:$LINENO, Command: $BASH_COMMAND"" ERR
+trap 'echo "There is an error in line:$LINENO, Command: $BASH_COMMAND"' ERR
 
 mkdir -p /var/log/shell-script
 Logs_file="/var/log/shell-script/$0.log"
