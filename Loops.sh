0cfadd1ed3089b5a54fbfa313217853e706f76fc
Vinod <vinodsv1618@gmail.com>
2026-01-30 11:02:02 +0530

    hello

--- Loops.sh
+++ Loops.sh
@@ -2,6 +2,13 @@
 
 Logs_file="/var/log/shell-script/$0.log"
 
+validate (){
+    if [ $1 -ne 0 ]; then
+       echo "$2 installation is failed"
+   else 
+      echo "$2 installation is success"
+   fi
+      
 for package_name in $@
 do
    dnf install $package_name -y | tee -a $Logs_file
