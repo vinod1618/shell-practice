7af378531cdfc7e85116c10a14cafa395a4f72c4
Vinod <vinodsv1618@gmail.com>
2026-01-30 13:50:41 +0530

    hello

---
+++ set_command.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e 
+
+mkdir -p /var/log/shell-script
+Logs_file="/var/log/shell-script/$0.log"
+
+validate (){
+    if [ $1 -ne 0 ]; then
+       echo "$2 installation is failed"
+    else 
+      echo "$2 installation is success"
+    fi
+
+}
+
+for package_name in $@
+do
+   rpm -q $package_name &>>$Logs_file
+   if [ $? -eq 0 ]; then
+     echo "$package_name is already installed" | tee -a $Logs_file
+   else 
+     dnf install $package_name -y | tee -a $Logs_file
+     #validate $? "$package_name"
+   fi
+done
